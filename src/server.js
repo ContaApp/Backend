707ca64c0cdcf921cac5b5400c87136f47fdcd04
Aldahir Mayorga Garcia <samg.25sep@gmail.com>
2022-04-07 10:28:03 -0500

    Arquitectura limpia y conexión a BD

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// Guardará la definción de mi servidor
+// configurar los middlewares
+// montar los routers
+
+const express = require('express')
+
+
+
+const server = express()
+
+
+// middlewares
+
+
+// routers
+
+
+
+
+module.exports = server
